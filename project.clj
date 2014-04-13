3d875eb3a4ce248eb4f6a072d4e32ef8c6bad489
Timothy Washington <twashing@gmail.com>
2014-04-12 22:26:57 -0700

    -- fixing project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject missing-utils "0.1.1-SNAPSHOT"
-  :description "Some missing utilities that I need across projects"
+  :description "Some missing functions that I need across projects"
   :url "https://github.com/twashing/missing-utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
