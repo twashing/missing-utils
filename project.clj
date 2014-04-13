e377addaddb6a15f000a327008fd69e34a3422a6
Timothy Washington <twashing@gmail.com>
2014-04-12 22:28:08 -0700

    lein-release plugin: bumped version from 0.1.1 to 0.1.2-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject missing-utils "0.1.1"
+(defproject missing-utils "0.1.2-SNAPSHOT"
   :description "Some missing functions that I need across projects"
   :url "https://github.com/twashing/missing-utils"
   :license {:name "Eclipse Public License"
