9c571a5bf0ee2f7b6a1386ff2a971e6824d10736
Timothy Washington <twashing@gmail.com>
2014-04-12 22:25:05 -0700

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject missing-utils "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject missing-utils "0.1.1-SNAPSHOT"
+  :description "Some missing utilities that I need across projects"
+  :url "https://github.com/twashing/missing-utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  
+  :lein-release {:deploy-via :clojars})
