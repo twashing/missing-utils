576a06205018b0e85632ce5e70540ae06ec576df
Timothy Washington <twashing@gmail.com>
2014-05-11 16:55:56 -0700

    -- adding functions to see java methods

--- src/missing_utils/core.clj
+++ src/missing_utils/core.clj
@@ -1,14 +1,17 @@
 (ns missing-utils.core
   (:require [clojure.java.io :as io]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+
+            [clojure.reflect :as r]
+            [clojure.pprint :refer [print-table]]))
 
 
-(defn generate-id 
+(defn generate-id
   "generate system wide unique ID"
-  []  
+  []
   (.. (java.rmi.dgc.VMID.) toString (replaceAll ":" "") (replaceAll "-" "")))
 
-(defn list-fns-in-ns [input-ns]
+(defn fns-in-ns [input-ns]
   (require input-ns)
   (keys (ns-publics input-ns)))
 
@@ -16,4 +19,9 @@
   (let [pbreader (java.io.PushbackReader. (io/reader (io/resource fname)))]
     (edn/read pbreader)))
 
+(defn java-methods [^Object obj]
+  (sort-by :name
+           (filter :exception-types (:members (r/reflect client)))))
 
+(defn java-methods-table [^Object obj]
+  (clojure.pprint/print-table (java-methods obj)))
