fb138bf30c0ac5dc0ad6954a4560e9416c195034
Timothy Washington <twashing@interruptsoftware.com>
2014-08-08 15:08:42 +0000

    lein-release plugin: preparing 0.1.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject missing-utils "0.1.2-SNAPSHOT"
+(defproject missing-utils "0.1.2"
   :description "Some missing functions that I need across projects"
   :url "https://github.com/twashing/missing-utils"
   :license {:name "Eclipse Public License"
