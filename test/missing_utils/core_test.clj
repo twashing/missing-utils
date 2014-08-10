49a239a6252a7c931e2a22339b49aa414cc12492
Timothy Washington <twashing@interruptsoftware.com>
2014-08-10 15:09:02 +0000

    -- removing unused test/ dir

--- test/missing_utils/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns missing-utils.core-test
-  (:require [clojure.test :refer :all]
-            [missing-utils.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
