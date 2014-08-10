ead3bdb41427ca8c23d870c98e7aa53055b66f7e
Timothy Washington <twashing@interruptsoftware.com>
2014-08-10 16:12:58 +0000

    lein-release plugin: bumped version from 0.1.2 to 0.1.3-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject missing-utils "0.1.2"
+(defproject missing-utils "0.1.3-SNAPSHOT"
   :description "Some missing functions that I need across projects"
   :url "https://github.com/twashing/missing-utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+
+  :local-repo-classpath true
   :lein-release {:deploy-via :clojars})
